6b492554fbcdfc92fa6d6b91a28a79fac2937794
MJ-1-2-3 <107686328+MJ-1-2-3@users.noreply.github.com>
2023-06-23 09:28:19 -0400

    dynamic tags

--- src/Filter.js
+++ src/Filter.js
@@ -1,12 +1,32 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect} from 'react'
 
 function Filter() {
-    const [checkboxes, setCheckboxes] = useState({
-        Sports: false,
-        Games: false,
-        Health: false,
-        Politics: false,
-    });
+    const [checkboxes, setCheckboxes] = useState({});
+    useEffect(() => {
+        fetchData();
+      }, []);
+
+    
+      const fetchData = async () => {
+        try {
+          const response = await fetch('http://127.0.0.1:8000/polls/get_tag/');
+          if (response.ok) {
+            const data = await response.json();
+            console.log('Tag Data:', data);
+            const updatedCheckboxes = {};
+
+            data.Tags.forEach((tag) => {
+                updatedCheckboxes[tag] = false;
+              });
+            setCheckboxes(updatedCheckboxes); 
+          } else {
+            throw new Error('Error fetching tag data');
+          }
+        } catch (error) {
+          console.error('Error fetching tag data:', error);
+        }
+      };
+    
 
     const handleCheckboxChange = (event) => {
         const { name, checked } = event.target;
@@ -24,46 +44,23 @@ function Filter() {
     return (
         <div style={{backgroundColor: 'gray', width:'100px'}}>
         <form onSubmit={handleSubmit}>
-        <label>
-        Sports:
-        <input
-            type='checkbox'
-            name='Sports'
-            checked={checkboxes.checkbox1}
-            onChange={handleCheckboxChange}
-            />
-        </label>
-        <br /><br/>
-        <label>
-        Games:
-        <input
-            type='checkbox'
-            name='Games'
-            checked={checkboxes.checkbox2}
-            onChange={handleCheckboxChange}
-            /> 
-        </label>
-        <br /><br/>
-        <label>
-        Health:
-        <input
-            type='checkbox'
-            name='Health'
-            checked={checkboxes.checkbox3}
-            onChange={handleCheckboxChange}
-            /> 
-        </label>
-        <br /><br/>
-        <label>
-        Politics:
-        <input
-            type='checkbox'
-            name='Politics'
-            checked={checkboxes.checkbox4}
-            onChange={handleCheckboxChange}
-            /> 
-        </label>
-        <br /><br/>
+        {Object.entries(checkboxes).map(([tag, isChecked]) => (
+          <div key={tag}>
+            <label>
+              {tag}:
+              <input
+                type="checkbox"
+                name={tag}
+                checked={isChecked}
+                onChange={handleCheckboxChange}
+              />
+            </label>
+            <br />
+            <br />
+          </div>
+        ))}   
+            
+   
         <button type='submit'>FilterbyTags</button>
 
         </form>
